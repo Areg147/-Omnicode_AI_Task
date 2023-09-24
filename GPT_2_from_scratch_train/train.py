ea9f669cdc823c30e7eeba54b7b2e6e27bc8a3d7
Areg <131033594+Areg147@users.noreply.github.com>
2023-09-24 22:01:45 +0400

    Update train.py

--- GPT_2_from_scratch_train/train.py
+++ GPT_2_from_scratch_train/train.py
@@ -14,6 +14,40 @@ class GPT(tf.keras.Model):
     
     def call(self,input_):
         return self.backbone(input_)["logits"]
+
+
+K = tf.keras.backend 
+
+class PerplexityMetric(tf.keras.metrics.Metric):
+
+    def __init__(self, name='perplexity', **kwargs):
+        super(PerplexityMetric, self).__init__(name=name, **kwargs)
+        self.cross_entropy = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True, reduction='none')
+        self.perplexity = self.add_weight(name='tp', initializer='zeros')
+
+    def _calculate_perplexity(self, real, pred):
+        mask = tf.math.logical_not(tf.math.equal(real, 0))
+        loss_ = self.cross_entropy(real, pred)
+        mask = tf.cast(mask, dtype=loss_.dtype)
+        loss_ *= mask
+        step1 = K.mean(loss_, axis=-1)
+        step2 = K.exp(step1)
+        perplexity = K.mean(step2)
+
+        return perplexity 
+
+
+    def update_state(self, y_true, y_pred, sample_weight=None):
+        
+        perplexity = self._calculate_perplexity(y_true, y_pred)
+
+        self.perplexity.assign_add(perplexity)
+        
+    def result(self):
+        return self.perplexity
+
+    def reset_states(self):
+        self.perplexity.assign(0.)
     
 ## Data Loading and Preparation
 ## defining all variables for preprocessor
@@ -21,7 +55,7 @@ class GPT(tf.keras.Model):
 csv_data_path = r"C:\Users\Dell\Desktop\Shekspir\data\Shakespeare_data.csv" #my example
 txt_data_path = r"C:\Users\Dell\Desktop\Shekspir\data\alllines.txt" #my example
 apprx_N_unique_words_for_train = 100000 #my example
-batch_size = 45 #my example
+batch_size = 32 #my example
 max_context_window = 35 #my example
 
 
@@ -33,7 +67,7 @@ data_for_training = pr.fit_transform()
 model = GPT()
 optimizer = tf.keras.optimizers.Adam()
 loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-metric = tf.keras.metrics.SparseCategoricalAccuracy('accuracy')
+metric = PerplexityMetric()
 
 model.compile(optimizer=optimizer, loss=tf.keras.losses.SparseCategoricalCrossentropy(), metrics=[metric])
 
